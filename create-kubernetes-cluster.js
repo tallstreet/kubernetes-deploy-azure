dbe5c60a7a3da281d75d30a04556f97d599fa72d
Gary <contact@tallstreet.com>
2015-10-06 21:43:07 +0100

    Update

--- create-kubernetes-cluster.js
+++ create-kubernetes-cluster.js
@@ -3,13 +3,15 @@
 var azure = require('./lib/azure_wrapper.js');
 var kube = require('./lib/deployment_logic/kubernetes.js');
 
-azure.create_config('kube', { 'etcd': 2, 'kube': 3 });
+azure.create_config('kube', { 'etcd': 2, 'kubemaster': 1, 'kubeworker': 2});
 
 azure.run_task_queue([
   azure.queue_default_network(),
   azure.queue_storage_if_needed(),
   azure.queue_machines('etcd', 'alpha',
     kube.create_etcd_cloud_config),
-  azure.queue_machines('kube', 'alpha',
-    kube.create_node_cloud_config),
+  azure.queue_machines('kubemaster', 'alpha',
+    kube.create_master_cloud_config),
+  azure.queue_machines('kubeworker', 'alpha',
+    kube.create_worker_cloud_config),
 ]);
