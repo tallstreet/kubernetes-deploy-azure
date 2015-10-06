bc88906c49401af3e6b5e4ecafc8ae84b26c3d5d
Gary Roberts <gary.roberts@smartfocus.com>
2015-10-06 12:47:49 +0100

    Updates

--- create-kubernetes-cluster.js
+++ create-kubernetes-cluster.js
@@ -3,13 +3,13 @@
 var azure = require('./lib/azure_wrapper.js');
 var kube = require('./lib/deployment_logic/kubernetes.js');
 
-azure.create_config('kube', { 'etcd': 3, 'kube': 3 });
+azure.create_config('kube', { 'etcd': 2, 'kube': 3 });
 
 azure.run_task_queue([
   azure.queue_default_network(),
   azure.queue_storage_if_needed(),
-  azure.queue_machines('etcd', 'stable',
+  azure.queue_machines('etcd', 'alpha',
     kube.create_etcd_cloud_config),
-  azure.queue_machines('kube', 'stable',
+  azure.queue_machines('kube', 'alpha',
     kube.create_node_cloud_config),
 ]);
