352cf5230d8196001038d7c8db60f5dfff3becdf
Gary Roberts <gary.roberts@smartfocus.com>
2015-10-08 13:22:41 +0100

    Add open ssl key generation

--- create-kubernetes-cluster.js
+++ create-kubernetes-cluster.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
 
 var azure = require('./lib/azure_wrapper.js');
+var util = require('./lib/util.js');
 var kube = require('./lib/deployment_logic/kubernetes.js');
 
-azure.create_config('kube', { 'etcd': 2, 'kubemaster': 1, 'kubeworker': 2});
+util.gen_keys(function() {
+  
+  azure.create_config('kube', { 'etcd': 2, 'kubemaster': 1, 'kubeworker': 2});
 
-azure.run_task_queue([
-  azure.queue_default_network(),
-  azure.queue_storage_if_needed(),
-  azure.queue_machines('etcd', 'alpha',
-    kube.create_etcd_cloud_config),
-  azure.queue_machines('kubemaster', 'alpha',
-    kube.create_master_cloud_config),
-  azure.queue_machines('kubeworker', 'alpha',
-    kube.create_worker_cloud_config),
-]);
+  azure.run_task_queue([
+    azure.queue_default_network(),
+    azure.queue_storage_if_needed(),
+    azure.queue_machines('etcd', 'alpha',
+      kube.create_etcd_cloud_config),
+    azure.queue_machines('kubemaster', 'alpha',
+      kube.create_master_cloud_config),
+    azure.queue_machines('kubeworker', 'alpha',
+      kube.create_worker_cloud_config),
+  ]);
+
+});
