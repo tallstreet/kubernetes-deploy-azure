62168ce68960d42ada79ae3b6c615480dc44c86a
Gary <contact@tallstreet.com>
2015-11-09 16:55:22 +0000

    Fix create node script

--- scale-kubernetes-cluster.js
+++ scale-kubernetes-cluster.js
@@ -3,8 +3,8 @@
 var azure = require('./lib/azure_wrapper.js');
 var kube = require('./lib/deployment_logic/kubernetes.js');
 
-azure.load_state_for_resizing(process.argv[2], 'kube', parseInt(process.argv[3] || 1));
+azure.load_state_for_resizing(process.argv[2], 'kubeworker', parseInt(process.argv[3] || 1));
 
 azure.run_task_queue([
-  azure.queue_machines('kubeworker', 'alpha', kube.create_worker_cloud_config),
+  azure.queue_machines('kubeworker', 'alpha', function() { return './output/kubernetes-cluster-worker-nodes_d2f6f75c2cd206_generated.yml' }),
 ]);
