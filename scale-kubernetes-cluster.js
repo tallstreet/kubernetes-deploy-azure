b54abcfd03aca94e9a3b69c128ceff5af766f97d
Gary <contact@tallstreet.com>
2015-12-30 10:18:29 +0000

    Update coreos version

--- scale-kubernetes-cluster.js
+++ scale-kubernetes-cluster.js
@@ -6,5 +6,5 @@ var kube = require('./lib/deployment_logic/kubernetes.js');
 azure.load_state_for_resizing(process.argv[2], 'kubeworker', parseInt(process.argv[3] || 1));
 
 azure.run_task_queue([
-  azure.queue_machines('kubeworker', 'alpha', function() { return process.arge[4] }),
+  azure.queue_machines('kubeworker', 'alpha', function() { return process.argv[4] }),
 ]);
